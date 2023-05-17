52aa80493ee00e35dd2a45d622c8110b14600355
Nery <pedronas@al.insper.edu.br>
2023-05-17 09:24:26 -0300

    musica

--- assets.py
+++ assets.py
@@ -3,4 +3,4 @@ import pygame
 # Define variáveis de imagem do jogo
 
 # Carrega sons do jogo
-theme_song = pygame.mixer.Sound('assets/snd/musica_tema.mp3')
+theme_song = pygame.mixer.music.load('assets/snd/musica_tema.ogg')
