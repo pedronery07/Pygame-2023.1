75e20892311994a582f844df1bcc51b220ce573b
Nery <pedronas@al.insper.edu.br>
2023-05-24 11:51:43 -0300

    reinício do projeto

--- assets.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-# Define variáveis de imagem do jogo
-
-# Carrega sons do jogo
-theme_song = pygame.mixer.music.load('assets/snd/musica_tema.ogg')
--- jogo.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from config import WIDTH, HEIGHT
-
-#Inicia o pygame
-pygame.init()
-
-#Configuração de tela e nome do jogo
-gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Tetris')
