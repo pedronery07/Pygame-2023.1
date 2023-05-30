ac070e2a54d3531919a912e19a0a01c98ad064ee
felipeap3 <felipeap3@al.insper.edu.br>
2023-05-30 11:46:18 -0300

    Elevando jogador

--- sprites.py
+++ sprites.py
@@ -13,7 +13,7 @@ class Ship(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.bottom = HEIGHT - 100
         self.speedx = 0
         self.groups = groups
         self.assets = assets
